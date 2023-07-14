6821cdbd19f425cd63fe55cfd385e407dbb800c0
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-13 23:44:53 -0300

    subir tarjetas

--- src/Pages/Sistemas.jsx
+++ src/Pages/Sistemas.jsx
@@ -1,12 +1,46 @@
 //Styles
 import styles from "../Module/Sistemas.module.css"
+//Imagenes
+import img from "../assets/img/1.png"
 
-const Sistemas = () => {
+const Sistemas = () => { 
     return (
         <>
          <div className={styles.container}> 
-	       En trabajo...
-         </div>
+          <section className={styles.tarjeta}>
+            <img src={img} alt="Foto" />
+            <div className={styles.informacion}>
+            <h3>Plan De Trabajo</h3>
+            <p>Precio $</p>
+            <span>
+                Detalles del plan
+            </span>
+            <button>Ver Mas</button>
+            </div>
+          </section>
+          <section className={styles.tarjeta}>
+            <img src={img} alt="Foto" />
+            <div className={styles.informacion}>
+            <h3>Plan De Trabajo</h3>
+            <p>Precio $</p>
+            <span>
+                Detalles del plan
+            </span>
+            <button>Ver Mas</button>
+            </div>
+          </section>
+          <section className={styles.tarjeta}>
+            <img src={img} alt="Foto" />
+            <div className={styles.informacion}>
+            <h3>Plan De Trabajo</h3>
+            <p>Precio $</p>
+            <span>
+                Detalles del plan
+            </span>
+            <button>Ver Mas</button>
+            </div>
+          </section>
+          </div>
         </>
     )
 }
