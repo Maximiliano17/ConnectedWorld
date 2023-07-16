02fe531d23e1987b8ab06f6a71c0bc5ab52847d5
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-15 21:38:07 -0300

    Subir tarjetas

--- src/Pages/Sistemas.jsx
+++ src/Pages/Sistemas.jsx
@@ -1,45 +1,38 @@
 //Styles
 import styles from "../Module/Sistemas.module.css"
 //Imagenes
-import img from "../assets/img/1.png"
+import img from "../assets/img/web.png"
+import img2 from "../assets/img/app.png"
+import img3 from "../assets/img/movile.png"
 
 const Sistemas = () => { 
-    return (
+    return ( 
         <>
-         <div className={styles.container}> 
-          <section className={styles.tarjeta}>
-            <img src={img} alt="Foto" />
+        <div className={styles.container}> 
+          <section className={styles.tarjeta}> 
+            <div className={styles.planes}>
+              <img src={img} alt="Imagen" />
             <div className={styles.informacion}>
-            <h3>Plan De Trabajo</h3>
-            <p>Precio $</p>
-            <span>
-                Detalles del plan
-            </span>
-            <button>Ver Mas</button>
+              <h3>Pagina Web</h3>
+              <a target="_blanck" href="https://www.mercadolibre.com.ar/">Ver Mas</a>
+            </div>
+            </div>
+              <div className={styles.planes}>
+                <img src={img2} alt="Imagen" />
+              <div className={styles.informacion}>
+                <h3>Aplicacion Web</h3>
+                <a target="_blanck" href="https://www.mercadolibre.com.ar/">Ver Mas</a>
+              </div>
+            </div>
+            <div className={styles.planes}>
+                <img src={img3} alt="Imagen" />
+              <div className={styles.informacion}>
+                <h3>Aplicacion Movil</h3>
+                <a target="_blanck" href="https://www.mercadolibre.com.ar/">Ver Mas</a>
+              </div>
             </div>
           </section>
-          <section className={styles.tarjeta}>
-            <img src={img} alt="Foto" />
-            <div className={styles.informacion}>
-            <h3>Plan De Trabajo</h3>
-            <p>Precio $</p>
-            <span>
-                Detalles del plan
-            </span>
-            <button>Ver Mas</button>
-            </div>
-          </section>
-          <section className={styles.tarjeta}>
-            <img src={img} alt="Foto" />
-            <div className={styles.informacion}>
-            <h3>Plan De Trabajo</h3>
-            <p>Precio $</p>
-            <span>
-                Detalles del plan
-            </span>
-            <button>Ver Mas</button>
-            </div>
-          </section>
+           
           </div>
         </>
     )
